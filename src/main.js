abe14d4ed69da52347e71745f313bf0f1e06b574
epiqu1n <epiqu1n@gmail.com>
2022-05-18 20:30:36 -0400

    Initial commit
    Co-authored-by: alrokosz <alrokosz@users.noreply.github.com>

---
+++ src/main.js
@@ -0,0 +1,21 @@
+(async () => {
+/*
+  // For importing component
+  const contentImageIrl = chrome.runtime.getURL("components/ContentImage.js");
+  const ContentImage = (await import(contentImageIrl)).default;
+ */
+
+/* 
+  // Replace 'contents' element with custom image
+  const contents = $("#contents");
+  const contentImg = new ContentImage();
+  contents.replaceWith(contentImg.img);
+ */
+
+/* 
+  // Add text before image
+  const text = $("<span>Get back to work!</span>").addClass("beautText");
+  text.insertBefore(contentImg.img);
+ */
+  chrome
+})();
